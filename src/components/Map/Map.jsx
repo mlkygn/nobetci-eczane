ba557c79eded8820fbf4e9eaf440100fd03a9745
Melike Y <melike@diji.tech>
2024-02-23 13:11:38 +0300

    map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,36 +7,46 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoibWxreWduIiwiYSI6ImNsc3V1eWVzYjEzNGMya211Ynhpam81NHcifQ.WKWqa7kqIdE6g2NQjKQK0g";
 
 function Map() {
-
-  const [position, setPosition] = useState({ latitude: null, longitude: null });
-  useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-      console.log(position.coords.latitude, position.coords.latitude);
-        setPosition({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-      });
-    } else {
-      console.log("Geolocation is not available in your browser.");
-    }
-  }, []);
-
+  const [userLoc, setuserLoc] = useState({
+    latitude: 40.1917,
+    longitude: 29.0611,
+  });
+  const [lat, setLat] = useState(userLoc.latitude);
+  const [lng, setLng] = useState(userLoc.longitude);
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lat, setLat] = useState(position.latitude);
-  const [lng, setLng] = useState(position.longitude);
-  const [zoom, setZoom] = useState(16);
+  const [zoom, setZoom] = useState(14);
 
-  useEffect(() => {
+  function getCurrentLoc() {
+    return new Promise((resolve) => {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          console.log(position.coords.latitude, position.coords.latitude);
+          setuserLoc({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        });
+        resolve();
+      } else {
+        console.log("Geolocation is not available in your browser.");
+      }
+    });
+  }
+  const mapInıtal = async () => {
     if (map.current) return; // initialize map only once
+    await getCurrentLoc();
+    setLat(userLoc.latitude);
+    setLng(userLoc.longitude);
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mlkygn/clsuv1q5d003v01qu1tsycp6f",
       center: [lng, lat],
       zoom: zoom,
     });
+  };
+  useEffect(() => {
+    mapInıtal();
   });
   return (
     <>
